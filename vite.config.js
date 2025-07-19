68abfd7509f60d19eed713e7490881cecee8e921
akinDenken <rafael.yang@unifesp.br>
2025-07-18 21:04:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     rollupOptions: {
       /* eslint-disable no-undef */
